6188b6cbbd3cc2b9ea9cd0e399e2c05226f2a7bc
lemy12 <bae9lomi@gmail.com>
2022-07-10 20:15:26 +0200

    funkcja error() i errorNumber()

--- skrypt.sh
+++ skrypt.sh
@@ -26,6 +26,30 @@ logsNumber() {
 	done
 }
 
+error() {
+	mkdir errors
+	cd errors
+	for i in {1..100}
+	do
+		mkdir error$i
+		echo "error$i.txt" >> error$i/error$i.txt
+		echo "$(basename $BASH_SOURCE)" >> error$i/error$i.txt
+		echo $(date) >> error$i/error$i.txt
+	done
+}
+
+errorNumber() {
+	mkdir errors
+	cd errors
+	for i in $(seq 1 $1)
+	do
+		mkdir error$i
+		echo "error$i.txt" >> error$i/error$i.txt
+		echo "$(basename $BASH_SOURCE)" >> error$i/error$i.txt
+		echo $(date) >> error$i/error$i.txt
+	done
+}
+
 pomoc() {
 	echo "--date - wyświetla obecną datę"
 	echo "--logs - tworzy katalog logs, a w nim 100 plików logów"
@@ -38,11 +62,19 @@ case $1 in
 		data
 		;;
 	--logs)
-		if [ -n $2 ]
+		if [ $# -eq 2 ]
 		then
 			logsNumber $2
 		else
 			logs
+		fi
+		;;
+	--error)
+		if [ $# -eq 2 ]
+		then
+			errorNumber $2
+		else
+			error
 		fi
 		;;
 	--help)
