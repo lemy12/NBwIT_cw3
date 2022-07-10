f64d5d82945ae4fd358220af39c3a5443cdd9f4b
lemy12 <bae9lomi@gmail.com>
2022-07-10 18:47:46 +0200

    obsluga argumentow i funkcja logs()

--- skrypt.sh
+++ skrypt.sh
@@ -4,4 +4,26 @@ data() {
 	echo $(date)
 }
 
-data
+logs() {
+	mkdir logs
+	cd logs
+	for i in {1..5}
+	do
+		touch log$i.txt
+		echo "log$i.txt" >> log$i.txt
+		echo "$(basename $BASH_SOURCE)" >> log$i.txt
+		echo $(date) >> log$i.txt
+	done
+}
+
+case $1 in
+	--date)
+		data
+		;;
+	--logs)
+		logs
+		;;
+	*)
+		echo "Podaj odpowiedni argument"
+		;;
+esac
